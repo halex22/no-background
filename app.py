e081a9881c3c50532eb6d39f955c60e34dc67a13
halex22 <73904950+halex22@users.noreply.github.com>
2024-11-25 11:21:09 +0100

    post method with image processor done

--- app.py
+++ app.py
@@ -6,17 +6,29 @@ from rembg import remove
 
 app = Flask(__name__)
 
+def process_file(file):
+    in_img = Image.open(file.stream)
+    out_img = remove(in_img, post_process_mask=True)
+    img_io = BytesIO()
+    out_img.save(img_io, 'PNG')
+    img_io.seek(0)
+    return img_io
+
+OUTPUT_NAME = '_no_background_img.png'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        print('post received')
         if 'file' not in request.files:
             return 'No file uploaded', 400
-        ...
-    
+        file = request.files['file']
+        if not file.filename:
+            return 'No file selected', 400
+        if file:
+            processed_img = process_file(file)
+            return send_file(processed_img, mimetype='image/png', as_attachment=True, download_name=OUTPUT_NAME)   
     return render_template('index.html')
 
-@app.route('/another-test')
+@app.route('/health-test')
 def test():
-    return "another test three"
+    return "I'm ok", 200
